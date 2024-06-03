1f5c27dcd635a5a68f3595524a005092a6ed9857
Pooja Dasharath Pawar <pawarsangita963@gmail.com>
2024-06-03 21:10:25 +0530

    Update script1.sh

--- script1.sh
+++ script1.sh
@@ -2,5 +2,7 @@ echo "We are trying this practical from linux EC2"
 echo "We are from batch-24 and learning git/gihub"
 echo "Happy weekend"
 echo "We are learning git"
+echo "Husband: I dont want to travel"
+
 
 
